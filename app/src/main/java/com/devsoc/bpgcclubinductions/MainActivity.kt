1b0e7ebc16ee670f838ab18745dd463d8bc59b40
ShivamHP <shivampachchigar14112@gmail.com>
2022-07-05 09:52:36 +0530

    Initial commit

---
+++ app/src/main/java/com/devsoc/bpgcclubinductions/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.devsoc.bpgcclubinductions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
